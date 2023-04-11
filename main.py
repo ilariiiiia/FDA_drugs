f5b9a9c37cc2ffdb9bd597464137ebe67339ea5d
nicola-commits <nicolamigone179@gmail.com>
2023-04-11 22:33:14 +0200

    Improved performance 1500x

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import json
 data = []
 
 applicantDict = {}
-tradeNameDict = {}
-drugNames = []
-drugTradenames = []
+matches = []
 
 def filePath(i):
     return f"./assets/drug-label-000{i}-of-0011.json" if i < 10 else f"./assets/drug-label-00{i}-of-0011.json"
@@ -14,30 +12,31 @@ def filePath(i):
 def colored(r, g, b, text):
     return f"\033[38;2;{r};{g};{b}m{text} \033[38;2;255;255;255m"
 
-def are_similar(n:str, o:str) -> bool:
-    name = n.upper().split(' ')
-    other = o.upper().split(' ')
+def minimize(name: list[str]) -> str:
+    name = name.upper().split(' ')
     for elem in ["LTD", "LLC", "CO", "PHARMA", "PHARMACEUTICAL"]:
         with contextlib.suppress(ValueError):
             name.remove(elem)
-        with contextlib.suppress(ValueError):
-            other.remove(elem)
-    return name == other
+    return '_'.join(name)
+
+def are_similar(n:str, o:str) -> bool:
+    return minimize(n) == minimize(o)
 
 def get_drug_info():
     with open('assets/drug_info.json') as f:
         return json.load(f)
     
 def generate_drug_info():
+
     for drug in get_drug_info():
-        if applicantDict.get(drug["Applicant"]):
-            applicantDict[drug["Applicant"]].append(drug)
+
+        minName = minimize(drug["Applicant"])
+
+        if applicantDict.get(minName):
+            applicantDict[minName].append(drug)
+            
         else:
-            applicantDict[drug["Applicant"]] = [drug]
-        if tradeNameDict.get(drug["Trade_Name"]):
-            tradeNameDict[drug["Trade_Name"]].append(drug)
-        else:
-            tradeNameDict[drug["Trade_Name"]] = [drug]
+            applicantDict[minName] = [drug]
 
 """
 FILE STRUCTURES:
@@ -99,12 +98,24 @@ import time
 
 generate_drug_info()
 
-# result.json is a WAY TOO BIG of a file (over 6GB), so it's impossible to open.
-# I will instead loop through the 11 different JSON files and, for each, try to find a match in the drug_info file.
+"""
+HOW THIS WORKS
+
+I have created a dictionary, applicantDict, which has as keys the "minified" version of the applicant's name,
+and as values a list of the drugs that have that applicant's name.
+
+For each file, I will then search whether the applicantDict contains the applicant's name in the keys, and if so,
+check whether the corresponding drug is the same as the one for the file. If it is, add both to a list called matches.
+
+"""
+
+startTime = time.time()
+
 for i in range(11):
     if i: print(f"[INFO] Finished file {i+1} in time {time.time() - startNewFile}")
     startNewFile = time.time()
     print(f"[INFO] Opened file {i+1}")
+    matchesFound = 0
 
     with open(filePath(i+1)) as drugLabel:
 
@@ -130,21 +141,20 @@ for i in range(11):
                 print(colored(255, 0, 0, "[ERROR] Could not find manufacturer name"))
                 continue
 
-            drug_info = get_drug_info()
+            applicantList:list = applicantDict.get(minimize(manufacturer))
 
-            for d in drug_info:
+            applicantList = applicantList or []
 
-                applicant = d['Applicant_Full_Name']
-                ingredient = d['Ingredient']
-                trade_name = d['Trade_Name']
+            for infoDrug in applicantList:
+                if minimize(infoDrug['Trade_Name']) == minimize(brandName):
+                    matches.append([drug, infoDrug])
+                    matchesFound += 1
+                    print(colored(0, 255, 0, "[SUCCESS] Found a match!"))
 
-                if brandName.upper() == trade_name.upper() and are_similar(applicant.upper(), manufacturer.upper()):
-                    print(brandName, trade_name, manufacturer, applicant)
-                    print("SUCCESS!")
-                    # exit(0)
-                    with open("matches.json", "r") as file:
-                        matches = json.load(file)
-                    with open("matches.json", "w") as file:
-                        json.dump(matches.append({"drug":drug, "drug_info":drug_info}), file)
+            timeNeeded = time.time() - startDrug
+            timeFile = time.time() - startNewFile
+            print(colored(0, 255, 255, f"[INFO] Finished drug n {j}/{totalDrugs} in time {round(timeNeeded, 3)}; average: {round(timeFile / (j+1), 4)}"))
+        
+        print(f"[INFO] Found {matchesFound} matches")
 
-            print(colored(0, 255, 255, f"[INFO] Finished drug n {j}/{totalDrugs} in time {round(time.time() - startDrug, 3)}; average: {round((time.time() - startNewFile) / (j+1), 4)}, ETA: {(time.time() - startNewFile) / (j+1) * (totalDrugs-j)}"))
+print(f"Found {len(matches)} matches in total in {time.time() - startTime} s")
